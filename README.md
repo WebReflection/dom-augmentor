9d46e785dece0407bbea43bbba7f7436a28413db
Andrea Giammarchi <a.giammarchi@eyeo.com>
2019-01-12 18:33:05 +0100

    added code coverage

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # dom-augmentor
 
+[![Build Status](https://travis-ci.com/WebReflection/dom-augmentor.svg?branch=master)](https://travis-ci.com/WebReflection/dom-augmentor) [![Coverage Status](https://coveralls.io/repos/github/WebReflection/dom-augmentor/badge.svg?branch=master)](https://coveralls.io/github/WebReflection/dom-augmentor?branch=master) ![WebReflection status](https://offline.report/status/webreflection.svg)
+
+
+
 This is exactly the same as the [augmentor](https://github.com/WebReflection/augmentor) module, except it provides automatically `connected` and `disconnected` effects per dom nodes, when the guard is an empty Array.
 
 Compatible with any function that returns a DOM node, or a fragment, or a hyperhtml like Wire instance.
---
+++ test/test.js
@@ -0,0 +1,129 @@
+require('basichtml').init();
+const {
+  default: augmentor,
+  useEffect,
+  useRef,
+  useState
+} = require('../cjs');
+
+const Button = () => augmentor(text => {
+  useEffect(
+    () => {
+      console.log('connected');
+      return () => console.log('disconnected');
+    },
+    []
+  );
+  const [i, increment] = useState(0);
+  const {current: button} = useRef(createButton);
+  button.textContent = `${text} ${i}`;
+  button.onclick = () => increment(i + 1);
+  return button;
+});
+
+const Unknown = () => augmentor(text => {
+  useEffect(
+    () => {
+      console.log('connected');
+      return () => console.log('disconnected');
+    },
+    []
+  );
+  const [i, increment] = useState(0);
+  return {
+    nodeType: 0,
+    valueOf() {
+      return createButton();
+    }
+  };
+});
+
+const Nope = () => augmentor(text => {
+  useEffect(
+    () => {
+      console.log('connected');
+      return () => console.log('disconnected');
+    },
+    []
+  );
+  return {};
+});
+
+const Maybe = () => augmentor(text => {
+  useEffect(
+    () => {
+      console.log('connected');
+      return () => console.log('disconnected');
+    }
+  );
+  return {};
+});
+
+const Never = () => augmentor(text => {
+  useEffect(
+    () => {
+      console.log('connected');
+      return () => console.log('disconnected');
+    },
+    [1]
+  );
+  return {};
+});
+
+const Many = () => augmentor(text => {
+  useEffect(
+    () => {
+      console.log('connected');
+      return () => console.log('disconnected');
+    },
+    []
+  );
+  const fragment = document.createDocumentFragment();
+  fragment.appendChild(document.createTextNode());
+  fragment.appendChild(createButton()).textContent = text;
+  return fragment;
+});
+
+const Thrower = () => augmentor(() => {
+  useEffect(
+    () => {
+      console.log('connected');
+      return () => console.log('disconnected');
+    },
+    []
+  );
+  return document.createDocumentFragment();
+});
+
+const button = Button()('hello');
+const unknown = Unknown()('darkness');
+Nope()();
+Maybe()();
+Never()();
+
+
+document.body.appendChild(button);
+document.body.appendChild(unknown);
+document.body.appendChild(Many()('test'));
+
+button.click();
+button.dispatchEvent(event('connected'));
+button.dispatchEvent(event('disconnected'));
+
+try {
+  Thrower()();
+  console.assert(false);
+} catch(ok) {
+  console.log('all good');
+}
+
+
+function createButton() {
+  return document.createElement('button');
+}
+
+function event(type) {
+  const e = document.createEvent('Event');
+  e.initEvent(type, false, false);
+  return e;
+}
